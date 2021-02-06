592953a2cfa5b73f5781570bfb44a27cfac21867
omerulucan16 <omerulucan@windowslive.com>
2021-02-06 12:46:45 +0300

    Country List

--- src/Reducer/index.js
+++
@@ -1,4 +0,0 @@
-const omer = () =>{
-
-}
-export default  omer;
---
+++ src/Reducers/index.js
@@ -0,0 +1,18 @@
+const INITAL_STATE = {
+    countryList : [],
+    selectedCountry : ""
+}
+const reducer = (state = INITAL_STATE, action) =>{
+    /*switch (action.type) {
+        case 'YAZI_LISTESI_GETIR':
+          debugger;
+          return { ...state, channelList: action.payload };
+        case "setSelectedDate":
+          debugger;
+          return { ...state, selectedDate: action.payload };
+        default:
+          return state;
+      }*/
+    return state;
+}
+export default  reducer;
